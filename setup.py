db6e4935ba2f4efb25e25934bc11c9cde56303db
Andre Anjos <andre.dos.anjos@gmail.com>
2014-01-19 15:38:08 +0100

    Remove last legacy bits, package now builds find

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
 
     install_requires=[
       'setuptools',
+      'xbob.io',
     ],
 
     namespace_packages=[
--- xbob/db/base/__init__.py
+++ xbob/db/base/__init__.py
@@ -6,7 +6,4 @@
 """The db package contains simplified APIs to access data for various databases
 that can be used in Biometry, Machine Learning or Pattern Classification."""
 
-from . import utils, driver, iris
-
-__all__ = [k for k in dir() if not k.startswith('_')]
-if 'k' in locals(): del k
+from . import utils, driver
