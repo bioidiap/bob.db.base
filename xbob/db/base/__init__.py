4e03cf6bc39de31f6ff5c6169c83b2ae823256b2
Andre Anjos <andre.dos.anjos@gmail.com>
2014-03-24 13:02:23 +0100

    Add Travis-CI build support

--- xbob/db/base/__init__.py
+++ xbob/db/base/__init__.py
@@ -6,4 +6,24 @@
 """The db package contains simplified APIs to access data for various databases
 that can be used in Biometry, Machine Learning or Pattern Classification."""
 
+import pkg_resources
+
+__version__ = __import__('pkg_resources').require(__name__)[0].version
+
+def get_config():
+  """Returns a string containing the configuration information.
+  """
+
+  import pkg_resources
+
+  packages = pkg_resources.require(__name__)
+  this = packages[0]
+  deps = packages[1:]
+
+  retval =  "%s: %s (%s)\n" % (this.key, this.version, this.location)
+  retval += "  - python dependencies:\n"
+  for d in deps: retval += "    - %s: %s (%s)\n" % (d.key, d.version, d.location)
+
+  return retval.strip()
+
 from . import utils, driver
